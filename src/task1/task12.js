ac93b6d3c9a9c3fa6c0b1edb5c8098bc83089ee7
bhatit2 <tanviips786@gmail.com>
2020-07-13 16:51:13 +0530

    implemented task2

---
+++ src/task1/task12.js
@@ -0,0 +1,13 @@
+const csv = require("csvtojson");
+const fs = require("fs");
+const path = require("path");
+const csvFilePath = path.resolve(__dirname, "../assets/nodejs-hw1-ex1.csv");
+const txtFilePath = path.resolve(__dirname, "../assets/output.txt");
+csv()
+  .fromFile(csvFilePath)
+  .then(jsonObj => {
+    fs.writeFile(txtFilePath, JSON.stringify(jsonObj), err => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  });
