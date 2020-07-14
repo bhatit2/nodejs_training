fae3b6eb23f2af2b8c178ccd1dbfc8586229148a
bhatit2 <tanviips786@gmail.com>
2020-07-14 11:06:06 +0530

    Update task2.js

--- src/week1/task2.js
+++ src/week1/task2.js
@@ -1,14 +1,24 @@
 const csv = require("csvtojson");
 const fs = require("fs");
 const path = require("path");
-const csvFilePath = path.resolve(__dirname, "../assets/nodejs-hw1-ex1.csv");
-const txtFilePath = path.resolve(__dirname, "../assets/output.txt");
-
-csv()
-  .fromFile(csvFilePath)
-  .then(jsonObj => {
-    fs.writeFile(txtFilePath, JSON.stringify(jsonObj, null, 2), err => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
+const csvFilePath = path.resolve(__dirname, "../csv/input.csv");
+const txtFilePath = path.resolve(__dirname, "../txt/output.txt");
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream(txtFilePath);
+const csvToJson = csv({
+  colParser: {
+    amount: "omit",
+    price: "number"
+  }
+})
+  .preFileLine((fileLineString, lineIdx) => {
+    if (lineIdx === 0) {
+      return fileLineString.toLowerCase();
+    }
+    return fileLineString;
+  })
+  .on("error", err => {
+    console.log(err);
   });
+
+readStream.pipe(csvToJson).pipe(writeStream);
